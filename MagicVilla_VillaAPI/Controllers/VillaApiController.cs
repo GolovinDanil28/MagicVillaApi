57e3a35ffcd9e9547f8db949c281f0ae33469318
GolovinDanil28 <golovindanil28@yandex.ru>
2023-01-28 14:08:09 +0500

    Add project files.

---
+++ MagicVilla_VillaAPI/Controllers/VillaApiController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet("GetVillas")]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("GetVilla/{id}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id == id);
+        }
+    }
+}
